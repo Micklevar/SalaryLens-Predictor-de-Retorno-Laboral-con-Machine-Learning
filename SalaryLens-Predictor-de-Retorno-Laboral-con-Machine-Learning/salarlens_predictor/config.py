4cc6c7a215ba3a84e47d7efddf79614951345b2a
Alejandro Santos <alejandrosantosgg1930@gmail.com>
2026-04-12 21:19:15 -0500

    feat: complete data quality notebook and finalize cleaned_df with target transformation

--- SalaryLens-Predictor-de-Retorno-Laboral-con-Machine-Learning/salarlens_predictor/config.py
+++ SalaryLens-Predictor-de-Retorno-Laboral-con-Machine-Learning/salarlens_predictor/config.py
@@ -30,3 +30,28 @@ try:
     logger.add(lambda msg: tqdm.write(msg, end=""), colorize=True)
 except ModuleNotFoundError:
     pass
+
+
+# src/config.py
+
+# Columnas numericas (continuas o discretas)
+NUMERIC_COLS = [
+    'experience_years',
+    'skills_count',
+    'certifications',
+    'salary'
+]
+
+# Columnas categoricas nominales (sin orden intrinseco)
+CATEGORICAL_COLS = [
+    'job_title',
+    'industry',
+    'location',
+]
+
+# Columnas categoricas ordinales (con un orden logico)
+ORDINAL_COLS = [
+    'education_level',
+    'company_size',
+    'remote_work',
+]
